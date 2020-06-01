a59d9f478167543c8c7e787068ef6cef0da1e5c0
Jason Xu <xujason419@outlook.com>
2020-06-01 18:00:09 -0400

    add build 4

---
+++ build/precache-manifest.63f78ceea03aa54f4eb1a927d4049b92.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c7b3234b3c3ed488d2af8bb8867c979",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d0b1f2f2167fd026899d",
+    "url": "/static/css/main.56588b7d.chunk.css"
+  },
+  {
+    "revision": "3e601e6cfa1440bebe19",
+    "url": "/static/js/2.8801256b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.8801256b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0b1f2f2167fd026899d",
+    "url": "/static/js/main.aec38651.chunk.js"
+  },
+  {
+    "revision": "b396d0fc20b42dc2015f",
+    "url": "/static/js/runtime-main.36a1111f.js"
+  },
+  {
+    "revision": "055e1a23237fd7c9e460318284d4873a",
+    "url": "/static/media/AuthorProfile.055e1a23.png"
+  }
+]);
